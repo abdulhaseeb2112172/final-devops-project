9b4085888fa49541a456296437f659b30483b388
abdulhaseeb2112172 <bscs2112172@szabist.pk>
2025-06-28 23:38:39 +0500

    main.tf

--- main.tf
+++ main.tf
@@ -2,52 +2,52 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "haseeb_rg" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_virtual_network" "haseeb_vnet" {
+resource "azurerm_virtual_network" "vnet" {
   name                = "haseeb-vnet"
-  address_space       = ["10.100.0.0/16"]
+  address_space       = ["10.2.0.0/16"]
   location            = var.location
-  resource_group_name = azurerm_resource_group.haseeb_rg.name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "haseeb_subnet" {
+resource "azurerm_subnet" "subnet" {
   name                 = "haseeb-subnet"
-  resource_group_name  = azurerm_resource_group.haseeb_rg.name
-  virtual_network_name = azurerm_virtual_network.haseeb_vnet.name
-  address_prefixes     = ["10.100.1.0/24"]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.2.1.0/24"]
 }
 
-resource "azurerm_public_ip" "haseeb_public_ip" {
+resource "azurerm_public_ip" "public_ip" {
   name                = "haseeb-public-ip"
   location            = var.location
-  resource_group_name = azurerm_resource_group.haseeb_rg.name
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "haseeb_nic" {
+resource "azurerm_network_interface" "nic" {
   name                = "haseeb-nic"
   location            = var.location
-  resource_group_name = azurerm_resource_group.haseeb_rg.name
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "haseeb-ipconfig"
-    subnet_id                     = azurerm_subnet.haseeb_subnet.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.haseeb_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "haseeb_vm" {
+resource "azurerm_linux_virtual_machine" "vm" {
   name                  = "haseeb-vm"
-  resource_group_name   = azurerm_resource_group.haseeb_rg.name
+  resource_group_name   = azurerm_resource_group.rg.name
   location              = var.location
   size                  = "Standard_B1s"
   admin_username        = var.admin_username
-  network_interface_ids = [azurerm_network_interface.haseeb_nic.id]
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
   admin_ssh_key {
     username   = var.admin_username
@@ -70,5 +70,5 @@ resource "azurerm_linux_virtual_machine" "haseeb_vm" {
 }
 
 output "public_ip_address" {
-  value = azurerm_public_ip.haseeb_public_ip.ip_address
+  value = azurerm_public_ip.public_ip.ip_address
 }
